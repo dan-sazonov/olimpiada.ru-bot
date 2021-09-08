8e215656a10165c452f2c580bf19fbcb013f366d
Daniel Sazonov <p.294803@gmail.com>
2021-09-08 23:59:39 +0300

    Create get_html

--- features.py
+++ features.py
@@ -2,20 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_title(url):
+def get_html(url, selector):
     response = requests.get(url.rstrip('/'))
     soup = BeautifulSoup(response.text, 'lxml')
 
-    out = soup.select('.headline_activity h1')
+    return soup.select(selector)
+
+
+def get_title(url):
+    out = get_html(url, '.headline_activity h1')
 
     return out[0].text if out else None
 
 
 def get_last_news(url):
-    response = requests.get(url.rstrip('/'))
-    soup = BeautifulSoup(response.text, 'lxml')
+    out = get_html(url, '#new_for_activity a.new_link:first-child')
 
-    out = soup.select('#new_for_activity a.new_link:first-child')
     if not out:
         return None, None
 
