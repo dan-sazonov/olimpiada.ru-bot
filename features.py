c8cbbb9d5974bc2fdb5a5a82d0852d0f28617d94
Daniel Sazonov <p.294803@gmail.com>
2021-09-10 01:35:57 +0300

    Add docstrings

--- features.py
+++ features.py
@@ -1,25 +1,44 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 
 
-def get_html(url, selector):
+def get_html(url: str, selector: str) -> element.ResultSet:
+    """
+    Returns the html code by selector and validate the url
+
+    :param url: url that needs to be parsed
+    :param selector: CSS-selector of the desired element
+    :return: HTML code
+    """
     try:
         response = requests.get(url.rstrip('/'))
     except requests.exceptions.ConnectionError or requests.exceptions.MissingSchema:
         # do something if url is wrong
-        return ''
+        return BeautifulSoup.ResultSet()
     soup = BeautifulSoup(response.text, 'lxml')
 
     return soup.select(selector)
 
 
-def get_title(url):
+def get_title(url: str) -> str:
+    """
+    Returns the name of the competition by the url
+
+    :param url: url of the competition
+    :return: title of this competition
+    """
     out = get_html(url, '.headline_activity h1')
 
     return out[0].text if out else ''
 
 
-def get_last_news(url):
+def get_last_news(url: str) -> tuple:
+    """
+    Returns the title of the latest news about this competition
+
+    :param url: url of the competition
+    :return: date of publishing and title of this news: (date, title)
+    """
     out = get_html(url, '#new_for_activity a.new_link:first-child')
 
     if not out:
@@ -32,7 +51,13 @@ def get_last_news(url):
     return date, title
 
 
-def get_calender(url):
+def get_calender(url: str) -> dict:
+    """
+    Returns calendar with the schedule of this competition
+
+    :param url: url of the competition
+    :return: dict: {'name of this round': 'date of the event'}
+    """
     i = 0
     calender = dict()
 
