e31c9b0f8da9088f40aba80b57b1161be9e30607
Daniel Sazonov <p.294803@gmail.com>
2021-09-11 20:53:13 +0300

    Create validate_url

--- features.py
+++ features.py
@@ -1,18 +1,30 @@
 import datetime
+from scrapper import get_title
 
-def validate_url(url: str) -> str:
+
+def validate_url(url: str) -> (str, int):
     """
-    Returns correct url if getting only id. If url is invalid, returns empty string
+    Returns correct url and id if getting only id. If url is invalid, returns empty string. Otherwise, returns url
 
     :param url: url that needs to be validated or id
-    :return: correct id or empty string
+    :return: (correct url or empty string, id of this event or empty string)
     """
-    # сайт, который мы парсим, решил прилечь. За 3 fucking дня до дэдлайна.
-    # todo допилю эту фичу, как olimpiada.ru поднимется
-    pass
+    if url.isdigit():
+        event_id = url
+        url = f'https://olimpiada.ru/activity/{event_id}'
+    if not (url.startswith('https://olimpiada.ru/activity/') and get_title(url)):
+        return '', 0
+    event_id = url.strip('/').split('/')[-1]
+    return url, int(event_id)
 
 
 def convert_date(date: str) -> datetime.date:
+    """
+    Returns some fucking piece of shit
+
+    :param date:
+    :return:
+    """
     # сайт, который мы парсим, решил прилечь. За 3 fucking дня до дэдлайна.
     # todo допилю эту фичу, как olimpiada.ru поднимется
     pass
--- test.py
+++ test.py
@@ -1,23 +1,32 @@
 import features
 import scrapper
+import requests
 
-testing = list(range(1, 6)) + [73, 153, 465]
+testing = list(range(1, 6)) + [73, 153, 465, 22]
 
-for i in range(1, 6):
+for i in testing:
     url = f'https://olimpiada.ru/activity/{i}'
     title = scrapper.get_title(url)
     date, news_title = scrapper.get_last_news(url)
     calender = scrapper.get_calender(url)
-    if title and date and news_title:
-        print(title)
-        print(date)
-        print(news_title)
-        if calender:
-            print(calender)
 
-        print()
+    print(url)
+    print(title)
+    print(date)
+    print(news_title)
+    print(calender)
+
+    print()
 # url = 'https://olimpiada.ru/activity/2'
 #
 # print(scrapper.get_title(url))
 # print(scrapper.get_last_news(url))
 # print(scrapper.get_calender(url))
+
+print(features.validate_url('https://olimpiada.ru/activity/2/fuckbjk/dick'))
+print(features.validate_url('https://olimpiada.ru/activity/22'))
+print(features.validate_url('https://olimpiada.ru/activity/73'))
+print(features.validate_url('https://olimpiada.ru/activity/153'))
+print(features.validate_url('1'))
+print(features.validate_url('https://olimpiada.ru'))
+print(features.validate_url('https://old-sud.ga'))
