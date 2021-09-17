c85fe2b2873b42e8074b021257dab1a44b72578c
Daniel Sazonov <p.294803@gmail.com>
2021-09-17 12:40:36 +0300

    ADMIN_ID should be a set of int

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ Set global constants here and maybe some function that configure something.
 "Why not json/csv/yaml/something else? - Fuck you, here's why".
 """
 
-ADMIN_ID = 385056286
+ADMIN_ID = {385056286, }
