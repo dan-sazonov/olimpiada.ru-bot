30283c84482b55dc064f83e5bd1237c6284bf039
Daniel Sazonov <p.294803@gmail.com>
2021-09-22 21:25:00 +0300

    Create MessagesText, MessagesText.statuses

--- handlers.py
+++ handlers.py
@@ -7,19 +7,60 @@ moved to other files: `features` or `database`.
 from aiogram import types
 from dispatcher import dp, bot
 import config
+import database
+
+
+class MessagesText:
+    """
+    Text of messages sending by the bot
+    """
+
+    def __init__(self, user_id=0, event_id=0):
+        """
+        Create attributes that include the message text
+        """
+        self.user_events = database.UserEvents(user_id)
+        self.db_event = database.DbEvent(event_id)
+
+        self.start_polling = 'Бот запущен'
+        self.stop_polling = 'Бот остановлен'
+        self.help = 'Простой эхо-бот, который будет постепенно допиливаться'
+        self.add_events = 'Введите иды олимпух или ссылки на каждую из них, по одной на строке'
+        self.add_events_done = 'Добавлено!'
+        self.add_events_failed = 'С дюбавлением некотрых ивентов возникли ошибки. Посмотрите все свои олимпухи, чтобы' \
+                                 ' понять, что к чему'
+        self.del_events = 'Введите иды олимпух или ссылки на каждую из них, по одной на строке'
+        self.del_events_done = 'Удалено!'
+        self.del_events_failed = 'С удалением некотрых ивентов возникли ошибки. Посмотрите все свои олимпухи, чтобы' \
+                                 ' понять, что к чему'
+        # self.statuses = ''
+        # self.next_rounds = ''
+        # self.last_news = ''
+        # self.all_events = ''
+        # self.calendar = ''
+
+    def statuses(self):
+        ans = ['Статусы олимпух:']
+        for event in self.user_events.get_statuses():
+            ans.append(f'{event[0]} - {event[1].lower()}.')
+        return '\n'.join(ans)
+
+
+messages = MessagesText(user_id=1)
+print(messages.statuses())
 
 
 async def start_message(_):
-    await bot.send_message(chat_id=config.ADMIN_ID, text='Бот запущен')
+    await bot.send_message(chat_id=config.ADMIN_ID, text=messages.start_polling)
 
 
 async def stop_message(_):
-    await bot.send_message(chat_id=config.ADMIN_ID, text='Бот остановлен')
+    await bot.send_message(chat_id=config.ADMIN_ID, text=messages.stop_polling)
 
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Простой эхо-бот, который будет постепенно допиливаться")
+    await message.reply(messages.help)
 
 
 @dp.message_handler()
